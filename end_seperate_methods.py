6ed014a19a68a857da40f9e17f74f5ee146844f6
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-08-15 01:37:23 +0530

    Add files via upload

---
+++ end_seperate_methods.py
@@ -0,0 +1,50 @@
+"Cancadarion" #to get space or multiple spaces between the string then this method is used. to join the mutiple string also the cancadation method is used.
+# print("pyt"+"hon") #python. joining the two strings is called cancadation.
+# print("super"+"5") #super5. if their is a int or float number convert into that into str and cancadate the both strings. this is method 1.
+# print("sper"+str(5)) #super5. Method 2. this all the without space one.
+"tow get single space or multiple spaces in cancadation"
+# print("roh","an") #roh an. by keeping (,) comma we can get the single space.
+# print("roh " "an") #roh an.  in this way also we can get the single space beteween the strings.
+# print("roh" " an") #roh an. this is also one method to get the single space between the strings.
+
+# print("rohan"+'  '+"kumar"+'  '+"ravula") #rohan  kumar  ravula. (Two spaces).  by adding the (+) or an empty sting in between the strings we can get the multinple spaces.
+
+"seperate"
+# print(1,2,3,4,5,sep=("  ")) #1  2  3  4  5. (two spaces by using seperate it is easy to give multiple spaces.) This sepertate method workds when their is a comma(,) between the strings.
+# print(1,2,3,4,5,sep=("@")) #1@2@3@4@5.
+
+"End" # this end is having with the only 2nd print statement only .
+# print("python",end=(""))
+# print("programming") #pythonprogramming. the end statement is interlinked with only with 2nd line only.
+
+# print("python",end=("@"))
+# print("programming") #python@programming.
+
+# print("python",end=("   "))
+# print("programming") #python   programming. this end statemet is only interlinked with only with the secon print statement only. it has no connection with the 3rd print statement.
+# print("version") #version
+
+# print("python",end=("  "))
+# print("programming",end=("  "))
+# print("version") #python  programming  version. when we keep end statement in second line also then it works with thrid line too vis versa.
+
+# print("python",end=(""))
+# print("code",end=(""))
+# print("Version") #pythoncodeVersion
+
+# "combination of end & seperate"
+# print(1,2,3,4,end=" ",sep="-")
+# print(5,6,7,8,sep=" ",end=".")
+# print(9,10) #1-2-3-4 5 6 7 8.9 10
+
+"\n"
+# print("python\n" "programming") #python next line programming
+# print("python" "\nprogramming")  #python next line programming
+# print("python\n" "\nprogramming")  #python next line empty  then programming
+
+# print("hello"*3) #hellohellohello
+# print("hello "*3) #hello hello hello
+# print("hello\n"*3) #by using this method we will get the answer in vrtically. but after printing 3 times hello in vertically at last also we will get the one empty line also.
+
+print("hello\n"*3,end="") #we will get no space at the end 
+
